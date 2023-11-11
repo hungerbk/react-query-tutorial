56d8b695f310f3c0e103857e583dae019153f7a2
matt <wootae.jung@beautyselection.co.kr>
2023-11-11 18:27:01 +0900

    리액트 쿼리 종속성 설치 및 app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { getTime } from "./api/getTime";
 
-function App() {
+export default function App() {
+  const [time, setTime] = useState("");
+  useEffect(() => {
+    getTime("Asia/Seoul").then((data) => {
+      setTime(`${data.hour}:${data.minute}:${data.seconds}`);
+    });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React Query</h1>
+      <p> 지금 시간 : {time}</p>
+      <button>캐시 무효화</button>
     </div>
   );
 }
-
-export default App;
