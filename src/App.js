cf127f009220c80b4aeb8d26c4b06022e6c8fa88
matt <wootae.jung@beautyselection.co.kr>
2023-11-13 13:20:59 +0900

    feat : react query 기본사용법

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { getTime } from "./api/getTime";
+import { useQuery } from "@tanstack/react-query";
 
 export default function App() {
-  const [time, setTime] = useState("");
-  useEffect(() => {
-    getTime("Asia/Seoul").then((data) => {
-      setTime(`${data.hour}:${data.minute}:${data.seconds}`);
-    });
+  const seoulTimeZone = "Asia/Seoul";
+  const { isPending, error, data } = useQuery({
+    queryKey: ["api", "time", { tz: seoulTimeZone }],
+    queryFn: () => getTime(seoulTimeZone),
   });
+
+  if (isPending)
+    return (
+      <div>
+        <p>로딩중...</p>
+      </div>
+    );
+
+  if (error) return <div>에러 발생 {error.message}</div>;
 
   return (
     <div>
       <h1>React Query</h1>
-      <p> 지금 시간 : {time}</p>
+      <p> 지금 시간 : {data.dateTime}</p>
       <button>캐시 무효화</button>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const client = new QueryClient();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={client}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
